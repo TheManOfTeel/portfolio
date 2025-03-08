efa14694eb1e0650aa4647e057a0ba4015506d08
Danny <themanofteel@icloud.com>
2025-03-08 18:09:10 -0500

    cleanup imports, add csp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatRippleModule } from '@angular/material/core';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSortModule } from '@angular/material/sort';
-import { MatStepperModule } from '@angular/material/stepper';
-import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -41,28 +27,12 @@ import { StateService } from './services/state.service';
     BrowserModule,
     AppRoutingModule,
     MatButtonModule,
-    MatInputModule,
-    MatTableModule,
-    MatSortModule,
-    MatPaginatorModule,
-    MatPaginatorModule,
-    MatSortModule,
     MatCardModule,
     MatToolbarModule,
-    MatDialogModule,
-    MatFormFieldModule,
     MatIconModule,
-    MatSelectModule,
     MatTabsModule,
     MatRippleModule,
     MatExpansionModule,
-    MatDividerModule,
-    MatStepperModule,
-    MatSidenavModule,
-    MatListModule,
-    MatRadioModule,
-    MatProgressBarModule,
-    MatProgressSpinnerModule,
   ],
   providers: [StateService],
   bootstrap: [AppComponent]
