a9a1e2ff367fd65816b8852dc63b85ee4a7047a6
Danny <themanofteel@icloud.com>
2025-03-09 18:55:15 -0400

    update project description

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -70,7 +70,7 @@ export class ProjectsComponent {
       ),
       new Project(
         'Server Inventory Audit',
-        'Checks the directories of a system and outputs the logs into a .txt file. The files can then be compared and the missing file names are displayed and outputted into another .txt file for easier viewing and logging purposes. Outputted files can be opened directly through the program for ease of use. There is a terminal version and a version with a GUI. Programmed using Python 3.',
+        'Checks the directories of a system and outputs the logs into a .txt file. The files can then be compared and the missing file names are displayed and outputted into another .txt file for easier viewing and logging purposes. Outputted files can be opened directly through the program for ease of use. There is a terminal version and a version with a GUI. This application was used to verify that all files were transferred after a server migration. Programmed using Python 3.',
         undefined,
         undefined,
         undefined,
