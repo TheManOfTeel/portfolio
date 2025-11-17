c545d5a9537e84d3c0eecb30eace10a0de189cae
Danny <themanofteel@icloud.com>
2025-11-16 21:20:02 -0500

    update cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  defaultCommandTimeout: 10000,
+  defaultCommandTimeout: 120000,
 
   e2e: {
     'baseUrl': 'http://localhost:4200',
