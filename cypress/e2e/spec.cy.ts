ecff6117a6be9e95aa824e2b9dce49a99d374c36
Danny <themanofteel@icloud.com>
2025-11-16 21:41:01 -0500

    cypress test updates

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,5 @@
 describe('Navigation Test', () => {
   it('Visits the initial project page and navigates', () => {
-    cy.wait(10000);
     cy.visit('/')
     cy.contains('Welcome')
     cy.contains('About')
@@ -8,22 +7,22 @@ describe('Navigation Test', () => {
 
     cy.get('button').contains('Mode').then(($btn) => {
       if ($btn.text() === 'Dark Mode') {
-        cy.get('.header > img').should('not.have.class', 'dark-img')
+        cy.get('.header').find('img').should('not.have.class', 'dark-img')
         cy.get('button').contains('Dark Mode').click()
         cy.get('body').should('have.class', 'dark-theme')
-        cy.get('.header > img').should('have.class', 'dark-img')
+        cy.get('.header').find('img').should('have.class', 'dark-img')
         cy.get('button').contains('Light Mode').click()
         cy.get('body').should('not.have.class', 'dark-theme')
-        cy.get('.header > img').should('not.have.class', 'dark-img')
+        cy.get('.header').find('img').should('not.have.class', 'dark-img')
       }
     else{
-        cy.get('.header > img').should('have.class', 'dark-img')
+        cy.get('.header').find('img').should('have.class', 'dark-img')
         cy.get('button').contains('Light Mode').click()
         cy.get('body').should('not.have.class', 'dark-theme')
-        cy.get('.header > img').should('not.have.class', 'dark-img')
+        cy.get('.header').find('img').should('not.have.class', 'dark-img')
         cy.get('button').contains('Dark Mode').click()
         cy.get('body').should('have.class', 'dark-theme')
-        cy.get('.header > img').should('have.class', 'dark-img')
+        cy.get('.header').find('img').should('have.class', 'dark-img')
       }
     })
 
@@ -43,8 +42,8 @@ describe('Navigation Test', () => {
     cy.get('button').contains('Collapse All').click()
     cy.get('app-projects').invoke('height').should('lte', 1000)
 
-    cy.get('*[class^="footer"]').should('exist')
-    cy.get('[href="https://www.linkedin.com/in/daniel-teel-a6465017b"]').should('exist').should('have.attr', 'target', '_blank')
-    cy.get('[href="https://github.com/TheManOfTeel"]').should('exist').should('have.attr', 'target', '_blank')
+    cy.get('.footer').should('exist')
+    cy.get('.footer').find('[href="https://www.linkedin.com/in/daniel-teel-a6465017b"]').should('exist').should('have.attr', 'target', '_blank')
+    cy.get('.footer').find('[href="https://github.com/TheManOfTeel"]').should('exist').should('have.attr', 'target', '_blank')
   })
 })
