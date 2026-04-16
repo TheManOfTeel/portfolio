d765e961d964b13a7402259dfceeef3c127f48fa
Danny <themanofteel@icloud.com>
2026-04-15 21:09:38 -0400

    remove unused files

--- src/app/app.module.ts
+++
@@ -1,6 +0,0 @@
-import { NgModule } from '@angular/core';
-
-// This module is no longer used - the application now uses standalone bootstrap.
-// See main.ts for the bootstrap configuration and app.routes.ts for routing.
-@NgModule()
-export class AppModule { }
